860082abed0848ad470e437a86b3ca721cd6d6c7
Shalin Shah <shah4c1@uwindsor.ca>
2022-10-30 17:53:25 -0400

    New changes

---
+++ src/main/java/com/uwindsor/unnovate/aya/controller/KidsProfile.java
@@ -0,0 +1,100 @@
+package com.uwindsor.unnovate.aya.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.uwindsor.unnovate.aya.model.ChildDetails;
+import com.uwindsor.unnovate.aya.repository.ChildDetailsRepository;
+
+@RestController
+public class KidsProfile {
+	
+	@Autowired
+    private ChildDetailsRepository childRepository;
+
+	@RequestMapping(
+			  value = "/kidsprofile/create", 
+			  method = RequestMethod.POST, 
+			  headers = "Accept=application/json")
+	@ResponseBody
+	    public String createKidsDetails(@RequestBody String id) throws Exception {
+		 //create ObjectMapper instance
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        //read JSON file and convert to a customer object
+        ChildDetails customer = objectMapper.readValue(id, ChildDetails.class);
+
+        //print customer details
+        
+        childRepository.save(customer);
+        System.out.println(customer.toString());
+	        return "Hello World RESTful with Spring Boot:-  " +id;
+	    }
+	
+	@RequestMapping(
+			  value = "/kidsprofile/update", 
+			  method = RequestMethod.POST, 
+			  headers = "Accept=application/json")
+	@ResponseBody
+	    public String updateKidsDetails(@RequestBody String id) throws Exception {
+		 //create ObjectMapper instance
+      ObjectMapper objectMapper = new ObjectMapper();
+
+      //read JSON file and convert to a customer object
+      ChildDetails customer = objectMapper.readValue(id, ChildDetails.class);
+
+      //print customer details
+      System.out.println(customer.toString());
+	        return "Hello World RESTful with Spring Boot:-  " +id;
+	    }
+	
+	@RequestMapping(
+			  value = "/kidsprofile/delete", 
+			  method = RequestMethod.POST, 
+			  headers = "Accept=application/json")
+	@ResponseBody
+	    public String deleteKidsDetails(@RequestBody String id) throws Exception {
+		 //create ObjectMapper instance
+      ObjectMapper objectMapper = new ObjectMapper();
+
+      //read JSON file and convert to a customer object
+      ChildDetails customer = objectMapper.readValue(id, ChildDetails.class);
+
+      //print customer details
+      System.out.println(customer.toString());
+      customer.setId(Integer.parseInt(id));
+      childRepository.delete(customer);
+	        return "Hello World RESTful with Spring Boot:-  " +id;
+	    }
+	
+	@RequestMapping(
+			  value = "/kidsprofile/read", 
+			  method = RequestMethod.GET, 
+			  headers = "Accept=application/json")
+	@ResponseBody
+	    public String readKidsDetails(@RequestBody String id) throws Exception {
+		 //create ObjectMapper instance
+      ObjectMapper objectMapper = new ObjectMapper();
+
+      //read JSON file and convert to a customer object
+      ChildDetails customer = objectMapper.readValue(id, ChildDetails.class);
+      List <ChildDetails> list = childRepository.findAll();
+      //print customer details
+      for(int i=0;i<list.size();i++)
+      {
+    	  ChildDetails cd = list.get(i);
+    	  System.out.println("Child name is :- "+cd.getName());
+    	  System.out.println("Child Dotor name is :- "+cd.getDoctor_Name());
+      }
+      System.out.println(customer.toString()+" --- "+list.size());
+	        return "Hello World RESTful with Spring Boot:-  ";
+	    }
+
+}
--- src/main/java/com/uwindsor/unnovate/aya/model/ChildDetails.java
+++ src/main/java/com/uwindsor/unnovate/aya/model/ChildDetails.java
@@ -1,28 +1,91 @@
 package com.uwindsor.unnovate.aya.model;
 
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "child_details")
 public class ChildDetails {
 	
-	int id;
-	String name;
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
+	@Id
+	private int id;
+	private String Name;
+	private String Dob;
+	private String Gender;
+	private String Doctor_Name;
+	private String Doctor_Phone;
+	
 	
 	@Override
     public String toString() {
         return "ChildDetails {" +
                 " id= " + id +
-                " name= " + name 
+                " name= " + Name 
                 +" }";
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+	public String getName() {
+		return Name;
+	}
+
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+
+	public String getDateOfBirth() {
+		return Dob;
+	}
+
+
+	public void setDateOfBirth(String dateOfBirth) {
+		Dob = dateOfBirth;
+	}
+
+
+	public String getGender() {
+		return Gender;
+	}
+
+
+	public void setGender(String gender) {
+		Gender = gender;
+	}
+
+
+	public String getDoctor_Name() {
+		return Doctor_Name;
+	}
+
+
+	public void setDoctor_Name(String doctor_Name) {
+		Doctor_Name = doctor_Name;
+	}
+
+
+	public String getDoctor_Phone() {
+		return Doctor_Phone;
+	}
+
+
+	public void setDoctor_Phone(String doctor_Phone) {
+		Doctor_Phone = doctor_Phone;
 	}
 
 }
