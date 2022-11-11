b26a8f8780a78f56626216c200751ed9d15f05f5
Shalin Shah <shah4c1@uwindsor.ca>
2022-11-11 13:06:53 -0500

    Final changes

--- src/main/java/com/uwindsor/unnovate/aya/controller/LightController.java
+++ src/main/java/com/uwindsor/unnovate/aya/controller/LightController.java
@@ -1,5 +1,8 @@
 package com.uwindsor.unnovate.aya.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,9 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.uwindsor.unnovate.aya.model.ChildDetails;
+import com.uwindsor.unnovate.aya.model.LightStatus;
+import com.uwindsor.unnovate.aya.repository.ChildDetailsRepository;
+import com.uwindsor.unnovate.aya.repository.LightStatusRepository;
 
 @RestController
 public class LightController {
+	
+	@Autowired
+    private LightStatusRepository lightStatusRepository;
 	
 	@RequestMapping(
 			  value = "/lightstatus/changeLightStatus", 
@@ -19,15 +28,43 @@ public class LightController {
 	@ResponseBody
 	    public String changeLightStatus(@RequestBody String id) throws Exception {
 		 //create ObjectMapper instance
-	      ObjectMapper objectMapper = new ObjectMapper();
-	System.out.println(id);
-	      if(id.equals("0")) {
+		List<LightStatus> ls = lightStatusRepository.findAll();
+		String currentStatus="";
+		LightStatus cd = new LightStatus();
+		 for(int i=0;i<ls.size();i++)
+	      {
+			 cd = ls.get(i);
+			 currentStatus = cd.getStatus();
+	      }
+		 if(currentStatus.equals(id))
+			 return "Light Status Unchanged";
+		 else if(id.equals("0")) {
+			 cd.setStatus(id);
+			 lightStatusRepository.save(cd);
 	    	  System.out.println("Light Turned ON");
 		        return "Light Turned ON";}
 	      else {
+	    	  cd.setStatus(id);
+	    	  lightStatusRepository.save(cd);
 	    	  System.out.println("Light Turned OFF");
 	    	  return "Light Turned OFF";
 	      }
+		}
+	
+	@RequestMapping(
+			  value = "/lightstatus/currentLightStatus", 
+			  method = RequestMethod.GET)
+	@ResponseBody
+	    public String currentLightStatus(@RequestBody String id) throws Exception {
+		 //create ObjectMapper instance
+		List<LightStatus> ls = lightStatusRepository.findAll();
+		String currentStatus="";
+		 for(int i=0;i<ls.size();i++)
+	      {
+			 LightStatus cd = ls.get(i);
+			 currentStatus = cd.getStatus();
+	      }
+		return currentStatus;
 		}
 
 }
--- src/main/java/com/uwindsor/unnovate/aya/model/ChildDetails.java
+++ src/main/java/com/uwindsor/unnovate/aya/model/ChildDetails.java
@@ -13,78 +13,59 @@ public class ChildDetails {
 	
 	@Id
 	private int id;
-	private String Name;
-	private String Dob;
-	private String Gender;
-	private String Doctor_Name;
-	private String Doctor_Phone;
-	
+	private String name;
+	private String gender;
+	private String dob;
+	private String docid;
 	
 	@Override
     public String toString() {
         return "ChildDetails {" +
                 " id= " + id +
-                " name= " + Name 
+                " name= " + name 
                 +" }";
 	}
-
 
 	public int getId() {
 		return id;
 	}
 
-
 	public void setId(int id) {
 		this.id = id;
 	}
 
-
 	public String getName() {
-		return Name;
+		return name;
 	}
 
-
 	public void setName(String name) {
-		Name = name;
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
 	}
 
 	public String getDob() {
-		return Dob;
+		return dob;
 	}
-
 
 	public void setDob(String dob) {
-		Dob = dob;
+		this.dob = dob;
+	}
+
+	public String getDocid() {
+		return docid;
+	}
+
+	public void setDocid(String docid) {
+		this.docid = docid;
 	}
 
 
-	public String getGender() {
-		return Gender;
-	}
-
-
-	public void setGender(String gender) {
-		Gender = gender;
-	}
-
-
-	public String getDoctor_Name() {
-		return Doctor_Name;
-	}
-
-
-	public void setDoctor_Name(String doctor_Name) {
-		Doctor_Name = doctor_Name;
-	}
-
-
-	public String getDoctor_Phone() {
-		return Doctor_Phone;
-	}
-
-
-	public void setDoctor_Phone(String doctor_Phone) {
-		Doctor_Phone = doctor_Phone;
-	}
 
 }
